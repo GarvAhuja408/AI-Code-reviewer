36668768834494ec59293b33470cb2ecd57d080c
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-13 16:24:49 +0530

    Added name fixed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 function Navbar() {
     return (
         <nav className="navbar">
-            <h1>AI Code Reviewer</h1>
+            <h1>AI Code Reviewer by Garv Ahuja</h1>
             <span>Review • Fix • Understand</span>
         </nav>
     );
